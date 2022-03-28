c74d124f8ef4c098d05286ee5babd45d3566a948
Guaru34 <86795974+Guaru34@users.noreply.github.com>
2022-03-28 09:53:07 -0600

    miapi

---
+++ app/Controllers/Http/UsuariosController.ts
@@ -0,0 +1,27 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import Usuario from "App/Models/Usuario"
+
+export default class UsuariosController {
+  public async register({request,response}){
+    const email=request.input(['email'])
+    const pass=request.input(['password'])
+
+    const crear=new Usuario()
+    crear.email=email
+    crear.password=pass
+    crear.save()
+    return response.json({crear})
+  }
+  public async login({request,auth,response}){
+    const email = request.input('email')
+    const pass = request.input('password')
+
+    try {
+      const token = await auth.use('api').attempt(email, pass)
+      return token
+    } catch {
+      return response.badRequest('Invalid credentials')
+  }
+}
+}
