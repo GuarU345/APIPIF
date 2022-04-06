ec60559f4243310177e87b72f4fbc7f4461860b4
jovani1243 <acidis_123@hotmail.com>
2022-04-06 11:46:55 -0500

    abelardo

--- app/Validators/MensajesValidator.ts
+++ app/Validators/MensajesValidator.ts
@@ -1,6 +1,6 @@
 export default class MensajeRegisterValidator{
     public messages = {
-        unique: 'Ya existe en la base de datos el email',
+        unique: 'Ya existe el email',
         maxLength: 'Te has pasado del limite',
         minLength: 'Como minimo se requiere 8 caracteres en la contraseña'
     }
